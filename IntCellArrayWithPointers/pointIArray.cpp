9224c7bf62376b9c0a58cac9b050d7ec6c1dc29f
stefobark <barste20@fresh.evergreen.edu>
2015-10-08 11:08:07 -0700

    fixed some things

--- IntCellArrayWithPointers/pointIArray.cpp
+++ IntCellArrayWithPointers/pointIArray.cpp
@@ -1,5 +1,6 @@
 #include "./../TestIntCell/iCell.h"
 #include <iostream>
+#include<cstdio>
 using namespace std;
 
 /*
@@ -12,9 +13,9 @@ using namespace std;
 
 vector<iCell> iCells(int size);
 void printA(vector<iCell> iC);
-vector<iCell> & copyA(vector<iCell> iC);
+vector<iCell> copyA(vector<iCell> * iC);
 vector<iCell> copyB(const vector<iCell> & iC);
-
+int aSize = 99999;
 vector<iCell> iCells(int size){
 
 	vector<iCell> intcells;
@@ -31,10 +32,12 @@ void printA(vector<iCell> iC){
 	cout << "Original vector size: " << iC.size() << endl;
 }
 
-vector<iCell> * copyA(vector<iCell> iC){
-	vector<iCell> * n;
-	n = *iC;
-	return *n;
+vector<iCell> copyA(vector<iCell> iC){
+	vector<iCell> n;
+	for(int i = 0; i < iC.size(); i++){
+		n.push_back(iC[i]);
+	}
+	return n;
 }
 
 vector<iCell> copyB(const vector<iCell> & iC){
@@ -43,14 +46,14 @@ vector<iCell> copyB(const vector<iCell> & iC){
 }
 
 vector<iCell> copyC(vector<iCell> iC){
-	vector<iCell> newA; // an array of pointers to IntCells
-	newA = iC; // newA = address of iC
-	return newA; // dereference newA so we can return the value pointed to by newA
+	vector<iCell> newA;
+	newA = iC;
+	return newA; 
 }
 
 int main(){
 	
-	vector<iCell> theIntCell = iCells(10000);
+	vector<iCell> theIntCell = iCells(aSize);
 	cout << "\n\n***Things are starting to make sense***\n\n";
 	printA(theIntCell);
 	
@@ -72,8 +75,8 @@ int main(){
 	
 	cout << "\nBut, if we do things differently... with a copy function like this: \n\n";
 	cout << "\t vector<iCell> copyA(vector<iCell> iC){\n";
-	cout << "\t\tvector<iCell> * n = &iC";
-	cout << "\t\treturn *n";
+	cout << "\t\tvector<iCell>  n = iC\n";
+	cout << "\t\treturn n";
 	cout << "\n\t}\n\n";
 	
 	cout << "It passses in iC then, we have a pointer pointing at iC (but, is this pointing at a new iC? a local iC?\n";
@@ -83,29 +86,30 @@ int main(){
 	cout << "\nbut now, we can use this function to copy a vector-- then we can delete this copy and we will find that the original will be left intact.\n";
 
 	// try again with a new vector of iCells
-	vector<iCell> nextIntCells = iCells(10000);
+	vector<iCell> nextIntCells = iCells(aSize);
 	
 	t= clock();
-	vector<iCell> * newNext;
-	newNext = &copyA(nextIntCells);
+	vector<iCell> newNext;
+	newNext = copyA(nextIntCells);
 	t = clock() - t;
+
 	cout << "\n\n";
 	printf ("It took me %d clicks (%f seconds) to do copyA.\n",t,((float)t)/CLOCKS_PER_SEC);
 	
 	// clearing this new vector should not destroy the old vector
-	newNext->clear();
+	newNext.clear();
 	cout << "\nSee that this size is not 0: \n";
 	printA(nextIntCells);
 	
 	cout << "\nOr what if we pass the vector to the function as a constant reference? And return a constant reference to it's instance array of IntCells?:\n\n";
 	cout << "like this: \n\n";
 	cout << "\tvector<iCell> copyB(const vector<iCell> & iC){\n";
-	cout << "\t\tconst vector<iCell> & newA { iC }; // a constant reference to iC\n";
-	cout << "\t\treturn newA; // return the value pointed to by newA\n";
+	cout << "\t\tconst vector<iCell> & newA { iC };";
+	cout << "\t\treturn newA;\n";
 	cout << "\t}";
 	
 	t= clock();
-	vector<iCell> && moreNew = copyB(nextIntCells);
+	vector<iCell> moreNew = copyB(nextIntCells);
 	t = clock() - t;
 	printf ("\n\nIt took me %d clicks (%f seconds) to do copyB.\n",t,((float)t)/CLOCKS_PER_SEC);
 	
